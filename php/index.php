d5e3618f2393752dbfe04ea83bc3387f82a13ecb
rickknowlton <richard.d.knowlton@gmail.com>
2023-02-16 17:39:50 -0500

    v 2.0.1 -- boilerplate with blog functionality

--- php/index.php
+++ php/index.php
@@ -2,11 +2,12 @@
 <html>
 
 <head>
-    <title>Boilerplate PHP Site</title>
+    <title>PHP LAMP Stack Boilerplate</title>
 </head>
 
 <body>
-    <h1>This is a basic PHP LAMP Stack Boilerplate</h1>
+    <h1>PHP LAMP Stack Boilerplate - with Blog</h1>
+    <h3>This is a basic PHP LAMP Stack Boilerplate with blog functionality via phpMyAdmin</h3>
     <p>
         <?php
         $host = 'db';
@@ -16,14 +17,24 @@
 
         $conn = new mysqli($host, $user, $password, $db);
 
+        $query = 'SELECT * FROM blog';
+        $result = mysqli_query($conn, $query);
+
         if ($conn->connect_error) {
-            echo "Error connecting to database" . $conn->error;
+            echo "Error connecting to database!" . $conn->error;
             echo "<br>";
             echo "Connected " . date("m-d-Y") . " at " . date("h:i:sa");
         } else {
-            echo "Successfully connected to database";
+            echo "Successfully connected to database!";
             echo "<br>";
             echo "Connected " . date("m-d-Y") . " at " . date("h:i:sa");
+        }
+
+        while ($record = mysqli_fetch_assoc($result)) {
+            echo "<hr>";
+            echo "<h2>" . $record["title"] . "</h2>";
+            echo "<p>" . $record["content"] . "</p>";
+            echo "<small>Posted: " . $record["date"] . "</small>";
         }
         ?>
     </p>
